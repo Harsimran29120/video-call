3367f92678e4d539cfbdaa2688a2c2e5dadb2e83
Harsimran29120 <72358179+Harsimran29120@users.noreply.github.com>
2021-01-11 14:42:20 +0100

    Add files via upload

---
+++ precache-manifest.109df814f8111325d365718377da8e5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9ff99ef03c9812a7dd12f9c189e446",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b3052eb2c9723e6bf03",
+    "url": "/static/css/main.fe7b8db6.chunk.css"
+  },
+  {
+    "revision": "4ab1f39e3ff15c601352",
+    "url": "/static/js/2.ed419a53.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ed419a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3052eb2c9723e6bf03",
+    "url": "/static/js/main.5ddf4580.chunk.js"
+  },
+  {
+    "revision": "851641979bc7df9bc62c",
+    "url": "/static/js/runtime-main.5209f24e.js"
+  }
+]);
